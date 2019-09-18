a91a0abf42d6778b09812168c709f57b73229237
Fatih BAHCECI <fatih_bah@hotmail.com>
2019-09-19 02:13:22 +0300

    First Files

---
+++ activities.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
+return [
+
+    // Pages
+    'page_create'                 => 'oluşturulmuş sayfa',
+    'page_create_notification'    => 'Sayfa Oluşturuldu',
+    'page_update'                 => 'güncellenmiş sayfa',
+    'page_update_notification'    => 'Sayfa Güncellendi',
+    'page_delete'                 => 'silinmiş sayfa',
+    'page_delete_notification'    => 'Sayfa silindi',
+    'page_restore'                => 'geri yüklenmiş sayfa',
+    'page_restore_notification'   => 'Sayfa geri yüklendi',
+    'page_move'                   => 'taşınmış sayfa',
+
+    // Chapters
+    'chapter_create'              => 'oluşturulmuş bölüm',
+    'chapter_create_notification' => 'Bölüm Oluşturuldu',
+    'chapter_update'              => 'güncellenmiş bölüm',
+    'chapter_update_notification' => 'Bölüm Güncellendi',
+    'chapter_delete'              => 'silinmiş bölüm',
+    'chapter_delete_notification' => 'Bölüm Silindi',
+    'chapter_move'                => 'taşınmış bölüm',
+
+    // Books
+    'book_create'                 => 'oluşturulmuş kitap',
+    'book_create_notification'    => 'Kitap Oluşturuldu',
+    'book_update'                 => 'güncellenmiş kitap',
+    'book_update_notification'    => 'Kitap Güncellendi',
+    'book_delete'                 => 'silinmiş kitap',
+    'book_delete_notification'    => 'Kitap Silindi',
+    'book_sort'                   => 'sıralanmış kitap',
+    'book_sort_notification'      => 'Kitap Successfully Re-sorted',
+
+    // Bookshelves
+    'bookshelf_create'                 => 'oluşturulmuş kitaplık',
+    'bookshelf_create_notification'    => 'Kitaplık Oluşturuldu',
+    'bookshelf_update'                 => 'güncellenmiş kitaplık',
+    'bookshelf_update_notification'    => 'Kitaplık Güncellendi',
+    'bookshelf_delete'                 => 'silinmiş kitaplık',
+    'bookshelf_delete_notification'    => 'Kitaplık Silindi',
+
+    // Other
+    'commented_on'                => 'hakkında yorum yaptı',
+];
---
+++ validation.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Validation Lines
+ * The following language lines contain the default error messages used by
+ * the validator class. Some of these rules have multiple versions such
+ * as the size rules. Feel free to tweak each of these messages here.
+ */
+return [
+
+    // Standard laravel validation lines
+    'accepted'             => ':attribute kabul edilmeli.',
+    'active_url'           => ':attribute geçerli URL değil.',
+    'after'                => ':attribute :date tarihinden sonraki bir tarih olmalı.',
+    'alpha'                => ':attribute sadece harf içerebilir.',
+    'alpha_dash'           => ':attribute sadece harfler, rakamlar, kısa çizgiler ve alt çizgiler içerebilir..',
+    'alpha_num'            => ':attribute sadece harf ve rakam içerebilir.',
+    'array'                => ':attribute bir dizi olmalıdır.',
+    'before'               => ':attribute :date tarihinden önceki bir tarih olmalı.',
+    'between'              => [
+        'numeric' => ':attribute :min ile :max arasında olmalı.',
+        'file'    => ':attribute :min ile :max kilobayt arasında olmalı.',
+        'string'  => ':attribute :min ile :max karakter arasında olmalı.',
+        'array'   => ':attribute :min and :max eleman arasında olmalı.',
+    ],
+    'boolean'              => 'The :attribute doğru (true) veya yanlış (false) olabilir.',
+    'confirmed'            => 'The :attribute confirmation does not match.',
+    'date'                 => 'The :attribute is not a valid date.',
+    'date_format'          => 'The :attribute does not match the format :format.',
+    'different'            => 'The :attribute and :other must be different.',
+    'digits'               => 'The :attribute must be :digits digits.',
+    'digits_between'       => 'The :attribute must be between :min and :max digits.',
+    'email'                => 'The :attribute must be a valid email address.',
+    'ends_with' => 'The :attribute must end with one of the following: :values',
+    'filled'               => 'The :attribute field is required.',
+    'gt'                   => [
+        'numeric' => 'The :attribute must be greater than :value.',
+        'file'    => 'The :attribute must be greater than :value kilobytes.',
+        'string'  => 'The :attribute must be greater than :value characters.',
+        'array'   => 'The :attribute must have more than :value items.',
+    ],
+    'gte'                  => [
+        'numeric' => 'The :attribute must be greater than or equal :value.',
+        'file'    => 'The :attribute must be greater than or equal :value kilobytes.',
+        'string'  => 'The :attribute must be greater than or equal :value characters.',
+        'array'   => 'The :attribute must have :value items or more.',
+    ],
+    'exists'               => 'The selected :attribute is invalid.',
+    'image'                => 'The :attribute must be an image.',
+    'image_extension'      => 'The :attribute must have a valid & supported image extension.',
+    'in'                   => 'The selected :attribute is invalid.',
+    'integer'              => 'The :attribute must be an integer.',
+    'ip'                   => 'The :attribute must be a valid IP address.',
+    'ipv4'                 => 'The :attribute must be a valid IPv4 address.',
+    'ipv6'                 => 'The :attribute must be a valid IPv6 address.',
+    'json'                 => 'The :attribute must be a valid JSON string.',
+    'lt'                   => [
+        'numeric' => 'The :attribute must be less than :value.',
+        'file'    => 'The :attribute must be less than :value kilobytes.',
+        'string'  => 'The :attribute must be less than :value characters.',
+        'array'   => 'The :attribute must have less than :value items.',
+    ],
+    'lte'                  => [
+        'numeric' => 'The :attribute must be less than or equal :value.',
+        'file'    => 'The :attribute must be less than or equal :value kilobytes.',
+        'string'  => 'The :attribute must be less than or equal :value characters.',
+        'array'   => 'The :attribute must not have more than :value items.',
+    ],
+    'max'                  => [
+        'numeric' => 'The :attribute may not be greater than :max.',
+        'file'    => 'The :attribute may not be greater than :max kilobytes.',
+        'string'  => 'The :attribute may not be greater than :max characters.',
+        'array'   => 'The :attribute may not have more than :max items.',
+    ],
+    'mimes'                => 'The :attribute must be a file of type: :values.',
+    'min'                  => [
+        'numeric' => 'The :attribute must be at least :min.',
+        'file'    => 'The :attribute must be at least :min kilobytes.',
+        'string'  => 'The :attribute must be at least :min characters.',
+        'array'   => 'The :attribute must have at least :min items.',
+    ],
+    'no_double_extension'  => 'The :attribute must only have a single file extension.',
+    'not_in'               => 'The selected :attribute is invalid.',
+    'not_regex'            => 'The :attribute format is invalid.',
+    'numeric'              => 'The :attribute must be a number.',
+    'regex'                => 'The :attribute format is invalid.',
+    'required'             => 'The :attribute field is required.',
+    'required_if'          => 'The :attribute field is required when :other is :value.',
+    'required_with'        => 'The :attribute field is required when :values is present.',
+    'required_with_all'    => 'The :attribute field is required when :values is present.',
+    'required_without'     => 'The :attribute field is required when :values is not present.',
+    'required_without_all' => 'The :attribute field is required when none of :values are present.',
+    'same'                 => 'The :attribute and :other must match.',
+    'size'                 => [
+        'numeric' => 'The :attribute must be :size.',
+        'file'    => 'The :attribute must be :size kilobytes.',
+        'string'  => 'The :attribute must be :size characters.',
+        'array'   => 'The :attribute must contain :size items.',
+    ],
+    'string'               => 'The :attribute must be a string.',
+    'timezone'             => 'The :attribute must be a valid zone.',
+    'unique'               => 'The :attribute has already been taken.',
+    'url'                  => 'The :attribute format is invalid.',
+    'uploaded'             => 'The file could not be uploaded. The server may not accept files of this size.',
+
+    // Custom validation lines
+    'custom' => [
+        'password-confirm' => [
+            'required_with' => 'Password confirmation required',
+        ],
+    ],
+
+    // Custom validation attributes
+    'attributes' => [],
+];
