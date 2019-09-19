9143a6dd2d7c47d5f4ac286b909fe4f51aad6cec
fatih <fatihbahceci@gmail.com>
2019-09-19 08:42:18 +0300

    #5 Better translation

--- activities.php
+++ activities.php
@@ -6,41 +6,41 @@
 return [
 
     // Pages
-    'page_create'                 => 'oluşturulmuş sayfa',
+    'page_create'                 => 'tarafından oluşturulan sayfa',
     'page_create_notification'    => 'Sayfa Oluşturuldu',
-    'page_update'                 => 'güncellenmiş sayfa',
+    'page_update'                 => 'tarafından güncellenen sayfa',
     'page_update_notification'    => 'Sayfa Güncellendi',
-    'page_delete'                 => 'silinmiş sayfa',
+    'page_delete'                 => 'tarafından silinen sayfa',
     'page_delete_notification'    => 'Sayfa silindi',
     'page_restore'                => 'geri yüklenmiş sayfa',
     'page_restore_notification'   => 'Sayfa geri yüklendi',
     'page_move'                   => 'taşınmış sayfa',
 
     // Chapters
-    'chapter_create'              => 'oluşturulmuş bölüm',
+    'chapter_create'              => 'tarafından oluşturulan bölüm',
     'chapter_create_notification' => 'Bölüm Oluşturuldu',
-    'chapter_update'              => 'güncellenmiş bölüm',
+    'chapter_update'              => 'tarafından güncellenen bölüm',
     'chapter_update_notification' => 'Bölüm Güncellendi',
-    'chapter_delete'              => 'silinmiş bölüm',
+    'chapter_delete'              => 'tarafından silinen bölüm',
     'chapter_delete_notification' => 'Bölüm Silindi',
     'chapter_move'                => 'taşınmış bölüm',
 
     // Books
-    'book_create'                 => 'oluşturulmuş kitap',
+    'book_create'                 => 'tarafından oluşturulan kitap',
     'book_create_notification'    => 'Kitap Oluşturuldu',
-    'book_update'                 => 'güncellenmiş kitap',
+    'book_update'                 => 'tarafından güncellenen kitap',
     'book_update_notification'    => 'Kitap Güncellendi',
-    'book_delete'                 => 'silinmiş kitap',
+    'book_delete'                 => 'tarafından silinen kitap',
     'book_delete_notification'    => 'Kitap Silindi',
     'book_sort'                   => 'sıralanmış kitap',
     'book_sort_notification'      => 'Kitap Successfully Re-sorted',
 
     // Bookshelves
-    'bookshelf_create'                 => 'oluşturulmuş kitaplık',
+    'bookshelf_create'                 => 'tarafından oluşturulan kitaplık',
     'bookshelf_create_notification'    => 'Kitaplık Oluşturuldu',
-    'bookshelf_update'                 => 'güncellenmiş kitaplık',
+    'bookshelf_update'                 => 'tarafından güncellenen kitaplık',
     'bookshelf_update_notification'    => 'Kitaplık Güncellendi',
-    'bookshelf_delete'                 => 'silinmiş kitaplık',
+    'bookshelf_delete'                 => 'tarafından silinen kitaplık',
     'bookshelf_delete_notification'    => 'Kitaplık Silindi',
 
     // Other
